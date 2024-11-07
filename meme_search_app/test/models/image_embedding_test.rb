039d73b07ccf1bc6401c43d7af415e9df5144d6b
Jeremy Watt <jermwatt@gmail.com>
2024-11-07 04:34:07 -0700

    first push

---
+++ meme_search_app/test/models/image_embedding_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ImageEmbeddingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ meme_search_app/test/models/image_path_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ImagePathTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
